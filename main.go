4fc71edef08bbbc8d4abfd9fa0e9e93bca150f64
abdelilah ouaadouch <gxgamer710@gmail.com>
2023-10-15 21:51:19 +0000

    writing csv to sheet

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
+	"io"
 	"os"
 	"path"
 	"strings"
+
+	"github.com/xuri/excelize/v2"
 )
 
 type csvInfos struct {
@@ -60,6 +64,56 @@ func main() {
 		}
 	}
 	fmt.Println(csvPathsAndNames[1])
+	for i,tableInfos := range csvPathsAndNames {
+		printTableInExcel(&)
+	}
 	// create excel
 	// file := excelize.NewFile(excelize.Options{})
+}
+
+func printTableInExcel(excelFile *excelize.File, infos csvInfos) {
+	// get file data
+	csvFile, err := os.Open(infos.path)
+	if err != nil {
+		fmt.Println("error opening file", err)
+		return
+	}
+	defer csvFile.Close()
+	// get reader
+	reader := csv.NewReader(csvFile)
+	// get sheet
+	_, err = excelFile.NewSheet(infos.sheetName)
+	if err != nil {
+		fmt.Println("error creating sheet", err)
+		return
+	}
+	// iterate over rows
+	row := 1
+	for {
+		// get data
+		record, err := reader.Read()
+		if err == io.EOF {
+			fmt.Println("error reading records", err)
+			break
+		}
+		if err != nil {
+			fmt.Println("error reading record 2:", err)
+			break
+		}
+		// get cell name from cords
+		cell, err := excelize.CoordinatesToCellName(1, row)
+		if err != nil {
+			fmt.Println("error getting cell name :", err)
+			break
+		}
+		// write row
+		if row == 1 {
+			if err := excelFile.SetSheetRow(infos.sheetName, cell, &record); err != nil {
+				fmt.Println("coudnt write first row : ", err)
+				break
+			}
+			row++
+			continue
+		}
+	}
 }
